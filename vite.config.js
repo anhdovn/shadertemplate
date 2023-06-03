fc7a6b5aaf6a733a195ca7cf0b7aaa9024d53e3b
anhdoursa <anh.do@ursa.vn>
2023-06-03 20:28:07 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 // vite.config.js
 import vitePluginString from "vite-plugin-string";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "pages/flag/index.html"),
+      },
+    },
+  },
   plugins: [vitePluginString()],
-};
+});
