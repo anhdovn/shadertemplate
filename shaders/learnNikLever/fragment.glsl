8a94f036ad1046db766efc155979c2d1c8d239c4
anhdovn <ducanh.fcds.gtvt@gmail.com>
2023-08-30 10:42:14 +0700

    move reactangle

--- shaders/learnNikLever/fragment.glsl
+++ shaders/learnNikLever/fragment.glsl
@@ -1,6 +1,7 @@
 uniform vec2 uResolution;
 varying vec2 vUv;
 varying vec3 vPosition;
+uniform float uTime;
 
 float react(vec2 pos, vec2 size, vec2 center) {
     vec2 p = pos - center;
@@ -15,8 +16,8 @@ float react(vec2 pos, vec2 size, vec2 center) {
 
 void main() {
 
-    float inReact1 = react(vPosition.xy, vec2(0.25, 0.125), vec2(0.5, 0.75));
-    float inReact2 = react(vPosition.xy, vec2(0.25, 0.125), vec2(-0.5, 0.75));
+    float inReact1 = react(vPosition.xy, vec2(0.25, 0.125), vec2(cos(uTime) * 0.5, sin(uTime) * 0.5));
+    float inReact2 = react(vPosition.xy, vec2(0.25, 0.125), vec2(-cos(uTime) * 0.5, sin(uTime) * 0.5));
     vec2 color = vec2(1.0, 1.0) * inReact1 + vec2(1.0, 0.0) * inReact2;
 
     gl_FragColor = vec4(color, 0.0, 1.0);
