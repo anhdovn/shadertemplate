9773bd95a4c57b436ae8de661c37b50f9f9dc8d7
anhdovn <ducanh.fcds.gtvt@gmail.com>
2023-09-01 22:09:52 +0700

    rotation square

--- shaders/learnNikLever/fragment.glsl
+++ shaders/learnNikLever/fragment.glsl
@@ -1,24 +1,26 @@
-uniform vec2 uResolution;
-varying vec2 vUv;
 varying vec3 vPosition;
 uniform float uTime;
 
+mat2 getRotationMatrix(float theta) {
+    float s = sin(theta);
+    float c = cos(theta);
+    return mat2(-c, s, s, c);
+}
+
 float react(vec2 pos, vec2 size, vec2 center) {
-    vec2 p = pos - center;
-    float halfX = size.x * 0.5;
-    float halfY = size.y * 0.5;
-
-    float hoz = step(-halfX, p.x) - step(halfX, p.x);
-    float ver = step(-halfY, p.y) - step(halfY, p.y);
-
+    vec2 pt = pos - center;
+    float haftSizeX = size.x * 0.5;
+    float haftSizeY = size.y * 0.5;
+    float hoz = step(-haftSizeX, pos.x) - step(haftSizeX, pos.x);
+    float ver = step(-haftSizeY, pos.y) - step(haftSizeY, pos.y);
     return hoz * ver;
 }
 
 void main() {
+    mat2 mat = getRotationMatrix(uTime);
+    vec2 pos = vPosition.xy * mat;
 
-    float inReact1 = react(vPosition.xy, vec2(0.25, 0.125), vec2(cos(uTime) * 0.5, sin(uTime) * 0.5));
-    float inReact2 = react(vPosition.xy, vec2(0.25, 0.125), vec2(-cos(uTime) * 0.5, sin(uTime) * 0.5));
-    vec2 color = vec2(1.0, 1.0) * inReact1 + vec2(1.0, 0.0) * inReact2;
-
+    float inReact = react(pos, vec2(0.25), vec2(0.0));
+    vec2 color = vec2(1.0, 0.0) * inReact;
     gl_FragColor = vec4(color, 0.0, 1.0);
 }
